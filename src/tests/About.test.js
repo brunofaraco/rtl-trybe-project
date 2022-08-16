c6623bacf6d96905246ad7a5689516eb6c10672c
Bruno <brunocfaraco@hotmail.com.br>
2022-08-16 17:15:20 -0300

    conclusão do 1º teste do requisito nº 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import About from '../pages/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test "About" component', () => {
+  test('if it have info about Pokedex', () => {
+    //  Acessar
+    renderWithRouter(<About />);
+    // Agir
+    const pokedexParagraph = screen.getByText(/this application simulates a pokédex/i);
+
+    // Aferir
+    expect(pokedexParagraph).toBeInTheDocument();
+  });
+
+  // test('', () => {
+  //   //  Acessar
+
+  //   // Agir
+
+  //   // Aferir
+
+  // });
+});
