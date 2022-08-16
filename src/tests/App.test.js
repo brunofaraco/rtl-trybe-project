48f98db5dd3d7a8d44420f9666705e6df2e0ff0f
Bruno <brunocfaraco@hotmail.com.br>
2022-08-16 16:10:10 -0300

    conclusão do 1º teste do requisito nº 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import { toBeInTheDocument } from '@testing-library/jest-dom/dist/matchers';
+
+describe('Test "App" component', () => {
+  test('if nav links are in the top of "App"', () => {
+    //  Acessar
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    const favoritePokemonsLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    // Agir
+
+    // Aferir
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+  // test('should second', () => {
+  //   //  Acessar
+
+  //   // Agir
+
+  //   // Aferir
+  // });
+  // test('should third', () => {
+  //   //  Acessar
+
+  //   // Agir
+
+  //   // Aferir
+  // });
+  // test('should fourth', () => {
+  //   //  Acessar
+
+  //   // Agir
+
+  //   // Aferir
+  // });
+  // test('should fifth', () => {
+  //   //  Acessar
+
+  //   // Agir
+
+  //   // Aferir
+  // });
+});
