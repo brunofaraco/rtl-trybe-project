0c67c55cc4cb86aaaf5bc10a43a83cabe7622b61
Bruno <brunocfaraco@hotmail.com.br>
2022-08-16 22:59:15 -0300

    parando por hoje

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import Pokedex from '../pages/Pokedex';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test "Pokedex" component', () => {
+  test('', () => {
+    //  Acessar
+    renderWithRouter(<Pokedex />);
+    const POKEDEX_H2 = screen.getByRole(
+      'heading', { name: /encountered pokémons/i, level: 2 },
+    );
+
+    //  Agir
+    // Não foi necessário
+
+    //  Aferir
+    expect(POKEDEX_H2).toBeInTheDocument();
+  });
+
+  // test('', () => {
+  //   //  Acessar
+
+  //   //  Agir
+
+  //   //  Aferir
+  // });
+});
