5509ed75da730817d4024624db2f0ff95103ce80
Bruno <brunocfaraco@hotmail.com.br>
2022-08-17 19:08:43 -0300

    conclusão do requisito nº 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,23 +5,25 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Test "Pokemon" component', () => {
-  test('if the right card is rendered', () => {
+  test('if Pikachu card is rendered', () => {
     // Acessar
     renderWithRouter(<App />);
-    const POKEMON_NAME = screen.getByText(/pikachu/i);
-    const POKEMON_TYPE = screen.getAllByText(/electric/i)[0];
-    const POKEMON_WEIGHT = screen.getByText(/average/i);
     const MORE_DETAILS_LINK = screen.getByRole('link', { name: /more details/i });
+    expect(MORE_DETAILS_LINK).toBeInTheDocument();
+
+    // Agir
+    userEvent.click(MORE_DETAILS_LINK);
+
+    const POKEMON_NAME = screen.getAllByText(/pikachu/i)[1];
+    const POKEMON_TYPE = screen.getByText(/Electric/);
+    const POKEMON_WEIGHT = screen.getByText(/average/i);
     const PIKACHU_IMG = screen.getByRole('img', { name: /pikachu sprite/i });
     const PIKACHU_IMG_SRC = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
-    // Agir
-    // Não foi necessário
 
     // Aferir
     expect(POKEMON_NAME).toBeInTheDocument();
     expect(POKEMON_TYPE).toBeInTheDocument();
     expect(POKEMON_WEIGHT).toBeInTheDocument();
-    expect(MORE_DETAILS_LINK).toBeInTheDocument();
     expect(PIKACHU_IMG).toHaveAttribute('src', PIKACHU_IMG_SRC);
   });
 
