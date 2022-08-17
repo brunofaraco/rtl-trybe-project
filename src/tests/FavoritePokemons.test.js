d6d7385e86bc9f90863800eb36aeeae1b61d9698
Bruno <brunocfaraco@hotmail.com.br>
2022-08-16 22:20:42 -0300

    conclusão do requisito nº 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test Favorite Pokémons component', () => {
+  test('if the right message appears when no Pokémon is selected', () => {
+    // Acessar
+    renderWithRouter(<FavoritePokemons />);
+    const RIGHT_PARAGRAPH = screen.getByText(/no favorite/i);
+    // Agir
+
+    // Aferir
+    expect(RIGHT_PARAGRAPH).toBeInTheDocument();
+  });
+
+  // test('', () => {
+  //   // Acessar
+
+  //   // Agir
+
+  //   // Aferir
+  // });
+});
